9966205f25e06a2e5d2e6a6f9c782d634fc822f1
Laura Bailey <laurab@adaptsolutions.com.au>
2020-03-12 16:32:24 +1100

    Added esri and cleaned up usings

--- EsriBugsWPF/MainWindow.xaml.cs
+++ EsriBugsWPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EsriBugsWPF
 {
@@ -29,7 +19,7 @@ namespace EsriBugsWPF
         {
             var pageType = (Type)e.Parameter;
             var page = (Page)Activator.CreateInstance(pageType);
-            var bugWindow = new Window() { Content = page };
+            var bugWindow = new Window { Content = page };
             bugWindow.Show();
         }
     }
